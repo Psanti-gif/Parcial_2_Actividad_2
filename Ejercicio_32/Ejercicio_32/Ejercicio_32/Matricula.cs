6a7ddb7b140243f91687925faba1aa6766aaa3b9
Psanti Pineda <psantixt12@gmail.com>
2024-10-30 21:44:53 -0500

    Cambios 2

--- Ejercicio_32/Ejercicio_32/Ejercicio_32/Matricula.cs
+++ Ejercicio_32/Ejercicio_32/Ejercicio_32/Matricula.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_32
+{
+    public class Matricula
+    {
+        public const int ValorCreditoNormal = 50000;
+        public static decimal CalcularValorMatricula(Estudiante estudiante)
+        {
+            decimal valorMatricula;
+            int creditosExtras = estudiante.Creditos > 20 ? estudiante.Creditos - 20 : 0;
+        }
+    }
+}
