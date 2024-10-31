fed51fa1bf1c730145d424de2f4d1a4eae5578c7
Psanti Pineda <psantixt12@gmail.com>
2024-10-30 21:41:43 -0500

    Cambios

---
+++ Ejercicio_32/Ejercicio_32/Ejercicio_32/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Ejercicio_32
+{
+    public class Estudiante
+    {
+        public int Estrato { get; set; }
+        public int Creditos { get; set; }
+
+        public Estudiante(int estrato, int creditos)
+        {
+            Estrato = estrato;
+            Creditos = creditos;
+        }
+    }
+
+}
